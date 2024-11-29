40a95c7b865bd5b8e6b0549de7feb6960fc1516c
Matthew <jwl42mql@163.com>
2024-11-29 05:55:28 -0800

    eval QwQ_32B

--- evaluate_from_local.py
+++ evaluate_from_local.py
@@ -19,7 +19,10 @@ max_new_tokens = 2048
 
 
 def load_mmlu_pro():
-    dataset = load_dataset("TIGER-Lab/MMLU-Pro")
+    if args.data_path == "hf_hub":
+        dataset = load_dataset("TIGER-Lab/MMLU-Pro")
+    else:
+        dataset = load_dataset("parquet", data_dir=args.data_path)
     test_df, val_df = dataset["test"], dataset["validation"]
     test_df = preprocess(test_df)
     val_df = preprocess(val_df)
@@ -31,9 +34,11 @@ def load_model():
                 tensor_parallel_size=torch.cuda.device_count(),
                 max_model_len=max_model_length,
                 trust_remote_code=True)
+    logging.info(f"Load model {args.model} complete!")
     sampling_params = SamplingParams(temperature=0, max_tokens=max_new_tokens,
                                         stop=["Question:"])
     tokenizer = transformers.AutoTokenizer.from_pretrained(args.model, trust_remote_code=True)
+    logging.info(f"Load tokenizer complete!")
     return (llm, sampling_params), tokenizer
 
 
@@ -103,7 +108,7 @@ def extract_answer(text):
     if match:
         return match.group(1)
     else:
-        print("1st answer extract failed\n" + text)
+        logging.info("1st answer extract failed\n" + text)
         return extract_again(text)
 
 
@@ -198,10 +203,9 @@ def eval_cot(subject, model, tokenizer, val_df, test_df, output_path):
 
 def main():
     model, tokenizer = load_model()
-    if not os.path.exists(save_result_dir):
-        os.makedirs(save_result_dir)
 
     full_test_df, full_val_df = load_mmlu_pro()
+    
     all_subjects = []
     for each in full_test_df:
         if each["category"] not in all_subjects:
@@ -216,7 +220,8 @@ def main():
                 if each.replace(" ", "_") in sub.replace(" ", "_"):
                     selected_subjects.append(sub)
     logging.info("selected subjects:\n" + "\n".join(selected_subjects))
-    print("selected subjects:\n" + "\n".join(selected_subjects))
+    # print("selected subjects:\n" + "\n".join(selected_subjects))
+    
     sta_dict = {}
     selected_subjects = sorted(selected_subjects)
     with open(os.path.join(summary_path), 'a') as f:
@@ -233,13 +238,13 @@ def main():
         sta_dict[subject]["accu"] = acc
         with open(os.path.join(summary_path), 'a') as f:
             f.write("Average accuracy {:.4f} - {}\n".format(sta_dict[subject]["accu"], subject))
+    
     total_corr, total_wrong = 0.0, 0.0
     for k, v in sta_dict.items():
         total_corr += v["corr"]
         total_wrong += v["wrong"]
     total_accu = total_corr / (total_corr + total_wrong + 0.000001)
     sta_dict["total"] = {"corr": total_corr, "wrong": total_wrong, "accu": total_accu}
-
     with open(os.path.join(summary_path), 'a') as f:
         f.write("\n------average acc sta------\n")
         weighted_acc = total_accu
@@ -254,25 +259,28 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ntrain", "-k", type=int, default=5)
     parser.add_argument("--selected_subjects", "-sub", type=str, default="all")
+    # parser.add_argument("--data_path", "-dp", type=str, default="hf_hub") # TIGER-Lab/MMLU-Pro
+    parser.add_argument("--data_path", "-dp", type=str, default="/home/shaohanh/qilongma/blob/datasets/MMLU-Pro/data")
     parser.add_argument("--save_dir", "-s", type=str, default="results")
     parser.add_argument("--global_record_file", "-grf", type=str,
                         default="eval_record_collection.csv")
-    parser.add_argument("--gpu_util", "-gu", type=str, default="0.8")
-    parser.add_argument("--model", "-m", type=str, default="meta-llama/Llama-2-7b-hf")
-
+    parser.add_argument("--gpu_util", "-gu", type=str, default="0.9")
+    # parser.add_argument("--model", "-m", type=str, default="meta-llama/Llama-2-7b-hf")
+    parser.add_argument("--model", "-m", type=str, default="/home/shaohanh/qilongma/blob/public_models/QwQ-32B-Preview")
     args = parser.parse_args()
+
     os.makedirs(args.save_dir, exist_ok=True)
     global_record_file = args.global_record_file
     save_result_dir = os.path.join(
         args.save_dir, "/".join(args_generate_path(args))
     )
+    os.makedirs(save_result_dir, exist_ok=True)
     file_prefix = "-".join(args_generate_path(args))
     timestamp = time.time()
     time_str = time.strftime('%m-%d_%H-%M', time.localtime(timestamp))
     file_name = f"{file_prefix}_{time_str}_summary.txt"
     summary_path = os.path.join(args.save_dir, "summary", file_name)
     os.makedirs(os.path.join(args.save_dir, "summary"), exist_ok=True)
-    os.makedirs(save_result_dir, exist_ok=True)
     save_log_dir = os.path.join(args.save_dir, "log")
     os.makedirs(save_log_dir, exist_ok=True)
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
@@ -282,5 +290,3 @@ if __name__ == "__main__":
                                   logging.StreamHandler(sys.stdout)])
 
     main()
-
-
